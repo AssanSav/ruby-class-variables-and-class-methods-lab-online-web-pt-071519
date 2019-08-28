a9fa6ed7be28798af1aebfb1996e9168ad29718b
assane savadogo <assane.savadogo81@gmail.com>
2019-08-28 06:20:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,13 @@
 require 'pry'
+
+class Song 
+  attr_accessor :name, :artist, :genre 
+  
+  @@count
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist 
+    @genre = genre
+  end
+end
